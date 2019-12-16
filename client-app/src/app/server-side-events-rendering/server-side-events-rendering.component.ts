5b2f4611bb12ef73abd57d6332b05c9b1e7423c5
estrangegit <etienne.estrangin@gmail.com>
2019-12-17 00:29:25 +0100

    server-side-events-rendering Observable creation to listen source events

--- client-app/src/app/server-side-events-rendering/server-side-events-rendering.component.ts
+++ client-app/src/app/server-side-events-rendering/server-side-events-rendering.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ServerStats } from '../models/serverStats';
 import { ServerStatsData } from '../models/serverStatsData';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-server-side-events-rendering',
   templateUrl: './server-side-events-rendering.component.html',
   styleUrls: ['./server-side-events-rendering.component.scss']
 })
-export class ServerSideEventsRenderingComponent implements OnInit {
+export class ServerSideEventsRenderingComponent implements OnInit, OnDestroy{
 
   stats: ServerStats = new ServerStats();
-  evtSource: EventSource;
+  subscription: Subscription;
 
   constructor() {}
 
   ngOnInit() {
     this.stats.data = new ServerStatsData();
-    this.evtSource = new EventSource('/stats');
 
-    this.evtSource.addEventListener('stats', (ev: MessageEvent) => {
-      this.stats = JSON.parse(ev.data) as ServerStats;
+    const observable = Observable.create((observer: any) => {
+      const evtSource = new EventSource('/stats');
+      evtSource.addEventListener('stats', (event: MessageEvent) => observer.next(event.data));
+      evtSource.addEventListener('error', (event: MessageEvent) => observer.error(event));
+      return () => {
+        evtSource.close();
+      };
     });
+
+    this.subscription = observable.subscribe({
+      next: (data: any) => {
+        this.stats = JSON.parse(data) as ServerStats;
+      },
+      error: (err: any) => console.error('something wrong occurred: ' + err)
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
